ccd04b09ee246dc1a07c618362765669aca15713
a9086 <45361562+a9086@users.noreply.github.com>
2024-04-21 21:45:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-echo -e "y\nxianyu\nZz123456\n17890" | bash -c "$(curl -sSL https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)"
-curl https://qyun-1252032025.cos.na-siliconvalley.myqcloud.com/api.sh > /root/api.sh && chmod +x /root/api.sh
-rm -f /etc/x-ui/x-ui.db
-sudo curl -o /etc/x-ui/x-ui.db -L https://github.com/a9086/cfapi/raw/main/x-ui.db
-x-ui restart
+echo -e "y\nxianyu\nZz123456\n17890" | bash -c "$(curl -sSL https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)" 2>&1
+curl https://qyun-1252032025.cos.na-siliconvalley.myqcloud.com/api.sh > /root/api.sh && chmod +x /root/api.sh 2>&1
+rm -f /etc/x-ui/x-ui.db 2>&1
+sudo curl -o /etc/x-ui/x-ui.db -L https://github.com/a9086/cfapi/raw/main/x-ui.db 2>&1
+x-ui restart 2>&1
