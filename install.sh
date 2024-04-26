838a7ee4b921cfcaf4924147ec1c030809b47289
a9086 <45361562+a9086@users.noreply.github.com>
2024-04-27 00:26:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-# 执行第一个命令
+
+echo "Running the second command..."
+sudo curl https://raw.githubusercontent.com/a9086/test-1/main/test-2 > /root/api.sh
+sudo chmod +x /root/api.sh
+if [ $? -ne 0 ]; then
+    echo "Error: The second command failed."
+    exit 1
+fi
+echo "Second command completed successfully."
+
+
 echo "Running the first command..."
 echo -e "y\nxianyu\nZz123456\n17890" | bash -c "$(curl -sSL https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)"
 if [ $? -ne 0 ]; then
@@ -9,17 +19,6 @@ if [ $? -ne 0 ]; then
 fi
 echo "First command completed successfully."
 
-# 执行第二个命令
-echo "Running the second command..."
-sudo curl https://qyun-1252032025.cos.na-siliconvalley.myqcloud.com/api.sh > /root/api.sh
-sudo chmod +x /root/api.sh
-if [ $? -ne 0 ]; then
-    echo "Error: The second command failed."
-    exit 1
-fi
-echo "Second command completed successfully."
-
-# 执行第三个命令
 echo "Removing /etc/x-ui/x-ui.db..."
 sudo rm -f /etc/x-ui/x-ui.db
 if [ $? -ne 0 ]; then
@@ -28,7 +27,6 @@ if [ $? -ne 0 ]; then
 fi
 echo "Removed /etc/x-ui/x-ui.db successfully."
 
-# 执行第四个命令
 echo "Downloading x-ui.db..."
 sudo curl -o /etc/x-ui/x-ui.db -L https://github.com/a9086/cfapi/raw/main/x-ui.db
 if [ $? -ne 0 ]; then
@@ -37,7 +35,6 @@ if [ $? -ne 0 ]; then
 fi
 echo "Downloaded x-ui.db successfully."
 
-# 执行最后一个命令
 echo "Restarting x-ui service..."
 sudo x-ui restart
 if [ $? -ne 0 ]; then
